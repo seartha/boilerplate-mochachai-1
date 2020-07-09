ac33371c16637ad1c7c4e7f60d3a4ac3899ef2bf
seartha <62803886+seartha@users.noreply.github.com>
2020-07-10 02:03:55 +0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -9,10 +9,13 @@ suite('Unit Tests', function(){
     /** assert.fail() will always fail. Change it into something more useful... **/
   
     /** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
-    test('#isNull, #isNotNull', function(){
-      assert.isNull(null, 'this is an optional error description - e.g. null is null');
-      assert.isNotNull( 1, '1 is not null');
-    });
+   test('#isNull, #isNotNull', function() {
+  assert.isNull(
+    null,
+    'this is an optional error description - e.g. null is null'
+  );
+  assert.isNotNull(1, '1 is not null');
+});
   
     /** 2 - Use assert.isDefined() or assert.isUndefined() to make the tests pass. **/
     test('#isDefined, #isUndefined', function(){
